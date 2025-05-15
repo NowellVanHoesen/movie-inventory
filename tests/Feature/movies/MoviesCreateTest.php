0c671986561a7fb71a51c60c76c546214cd30b69
Nowell VanHoesen <nowell@nvwebdev.com>
2025-05-15 16:54:00 -0400

    beginning of movies.create tests

---
+++ tests/Feature/movies/MoviesCreateTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use function Pest\Laravel\get;
+
+it('has movies.create page', function () {
+    get(route('movies.create'))
+        ->assertOk();
+});
+
+// movies.create
+    // displays field to search TMDB API for movie
+// movies.search
+    // displays list of movied returned from TMDB API for search term
+    // displays specific movie detail from TMDB API after clicking one of the returned movied

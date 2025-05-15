56d9af16b2367f0b3b58c25137622e3953025378
Nowell VanHoesen <nowell@nvwebdev.com>
2025-05-15 16:50:03 -0400

    added tests for about and contact pages

--- tests/Feature/PagesResponseTest.php
+++ tests/Feature/PagesResponseTest.php
@@ -6,3 +6,13 @@ it('gives a successful response for the home page', function () {
     get(route('home'))
         ->assertOk();
 });
+
+it('gives a successful response for the about page', function () {
+    get(route('about'))
+        ->assertOk();
+});
+
+it('gives a successful response for the contact page', function () {
+    get(route('contact'))
+        ->assertOk();
+});

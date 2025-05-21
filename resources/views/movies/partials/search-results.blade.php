5aea71f4047221bd01962e75ede6177af45ba223
Nowell VanHoesen <nowell@nvwebdev.com>
2025-05-20 20:40:56 -0400

    if release date isn't defined, it's not a movie, skip it. Ran into an issue where a movie collection was returned with movie search results

--- resources/views/movies/partials/search-results.blade.php
+++ resources/views/movies/partials/search-results.blade.php
@@ -1,5 +1,6 @@
 <div class="grid grid-cols-[repeat(auto-fill,minmax(185px,1fr))] gap-5 mt-6">
 	@foreach ($search_results->results as $movie)
+		@continue( ! isset($movie->release_date) )
 		@if ($local_results->contains($movie->id))
 			@include('movies.partials.select-movie-link', [ 'movie' => $local_results->find($movie->id)])
 		@else

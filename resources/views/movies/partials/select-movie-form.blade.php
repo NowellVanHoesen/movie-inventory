6a780dce88cb6e69d1267cf3196c3f599e345a28
Nowell VanHoesen <nowell@nvwebdev.com>
2025-05-21 21:04:53 -0400

    created a hidden input component for movie and series posters and swapped them out

--- resources/views/movies/partials/select-movie-form.blade.php
+++ resources/views/movies/partials/select-movie-form.blade.php
@@ -1,9 +1,9 @@
 <form method="POST" action="{{ route('movies.search') }}" class="block relative group border border-gray-900 rounded-xl max-w-[185px]">
 	@csrf
-	<input type="hidden" name="movie_id" value="{{ $movie->id }}" />
 	<button type="submit" class="cursor-pointer p-0" @disabled(empty( $movie->release_date ))>
 	</button>
 </form>
+		<x-form-hidden-input name="movie_id" value="{{ $movie->id }}" />
 		<x-item-poster :placeholder="is_null( $movie->poster_path )" poster_path="{{ $movie->poster_path }}" size="w185" />
 		<x-poster-overlay-detail
 			class="{{ is_null( $movie->poster_path ) ? '' : 'invisible group-hover:visible' }} text-left"
--- resources/views/series/partials/select-series-form.blade.php
+++ resources/views/series/partials/select-series-form.blade.php
@@ -1,10 +1,10 @@
 <form method="POST" action="{{ route('series.create') }}" class="block relative border border-gray-900 rounded-xl max-w-[185px] mb-auto">
 	@csrf
-	<input type="hidden" name="series_id" value="{{ $series->id }}" />
-	<input type="hidden" name="search_term" value="{{ $search_term }}" />
 	<button type="submit" class="cursor-pointer p-0">
 	</button>
 </form>
+		<x-form-hidden-input name="series_id" value="{{ $series->id }}" />
+		<x-form-hidden-input name="search_term" value="{{ $search_term }}" />
 		<x-item-poster :placeholder="is_null( $series->poster_path )" poster_path="{{ $series->poster_path }}" size="w185" />
 		<x-poster-overlay-detail
 			class="{{ is_null( $series->poster_path ) ? '' : 'invisible group-hover:visible' }} text-left"

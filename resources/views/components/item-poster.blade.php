125492e23e21cf96a09aab2af69971bba16f4094
Nowell VanHoesen <nowell@nvwebdev.com>
2025-06-11 14:05:27 -0400

    added correct max width for posters and placeholder images

--- resources/views/components/item-poster.blade.php
+++ resources/views/components/item-poster.blade.php
@@ -1,3 +1,11 @@
 @props(['placeholder' => false, 'poster_path', 'size'])
 
-<img src="{{ $placeholder ? config('tmdb.placeholder.poster') : 'https://image.tmdb.org/t/p/' . $size . $poster_path }}" class="border border-gray-900 rounded-xl max-w-[185px]" />
+@php
+$max_w = match ($size) {
+	'w92' => '92px',
+	'w154' => '154px',
+	default => '185px',
+}
+@endphp
+
+<img src="{{ $placeholder ? config('tmdb.placeholder.poster') : 'https://image.tmdb.org/t/p/' . $size . $poster_path }}" class="border border-gray-900 rounded-xl max-w-[{{ $max_w }}]" />

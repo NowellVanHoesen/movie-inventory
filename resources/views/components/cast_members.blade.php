ebab1e7ce7331ceae45488020d61f1e90e255da7
Nowell VanHoesen <nowell@nvwebdev.com>
2025-07-16 12:48:10 -0400

    another button style tweak

--- resources/views/components/cast_members.blade.php
+++ resources/views/components/cast_members.blade.php
@@ -7,7 +7,7 @@
 	<div class="flex items-center justify-between">
 		<h2 class="text-2xl">Cast Members</h2>
 		@if ( count( $cast ) > $display_limit )
-		<button @click="showAll = !showAll" class="relative cursor-pointer px-4 py-2 text-sm font-medium text-gray-300 bg-[#3e4b62] border border-gray-300 leading-5 rounded-md hover:bg-gray-800 hover:text-white focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 transition ease-in-out duration-150">
+		<button @click="showAll = !showAll" class="cursor-pointer px-4 py-2 text-sm font-semibold text-white bg-[#3e4b62] border border-transparent rounded-md hover:bg-[#333c50] focus:bg-[#333c50] focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2 transition ease-in-out duration-150">
 			<span x-text="showAll ? 'Show Less' : 'Show All'"></span>
 		</button>
 		@endif

1f4d80527d42cd0042539bd8f9073c0295a01771
Nowell VanHoesen <nowell@nvwebdev.com>
2025-09-15 14:06:34 -0400

    trying a case change, layout file isn't found on the production site

--- app/View/Components/CastMemberLayout.php
+++ app/View/Components/CastMemberLayout.php
@@ -5,7 +5,7 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
-class CastMemberLayout extends Component
+class CastmemberLayout extends Component
 {
     /**
      * Get the view / contents that represent the component.

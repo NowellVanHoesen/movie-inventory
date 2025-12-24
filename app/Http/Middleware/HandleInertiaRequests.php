0f90e0e55fce125f8422c06bdcccf9e65be51789
Nowell VanHoesen <nowell@nvwebdev.com>
2025-12-24 16:59:20 -0500

    added a user resource and updated the handle inertia requests to include the user info

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -35,9 +35,13 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $user = $request->user()?->toResource();
+
         return [
             ...parent::share($request),
-            //
+            'auth' => [
+                'user' => $user,
+            ],
         ];
     }
 }

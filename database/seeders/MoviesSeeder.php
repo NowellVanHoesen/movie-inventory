2b5db6c47a57a3dd2b4262136a58c8880ece99c1
Nowell VanHoesen <nowell@nvwebdev.com>
2025-12-29 20:47:59 -0500

    updated movie seeder to include genres and media types

--- database/seeders/MoviesSeeder.php
+++ database/seeders/MoviesSeeder.php
@@ -13,11 +13,15 @@ class MoviesSeeder extends Seeder
     public function run(): void
     {
         $movieSeeder = 'database/sql-files/movies.sql';
+        $movieGenresSeeder = 'database/sql-files/genre_movie.sql';
+        $movieMediaTypeSeeder = 'database/sql-files/media_type_movie.sql';
         $castSeeder = 'database/sql-files/cast_members.sql';
         $movieCastSeeder = 'database/sql-files/cast_member_movie.sql';
         $movieCollectionSeeder = 'database/sql-files/movie_collections.sql';
 
         DB::statement(file_get_contents($movieSeeder));
+        DB::statement(file_get_contents($movieGenresSeeder));
+        DB::statement(file_get_contents($movieMediaTypeSeeder));
         DB::statement(file_get_contents($castSeeder));
         DB::statement(file_get_contents($movieCastSeeder));
         DB::statement(file_get_contents($movieCollectionSeeder));

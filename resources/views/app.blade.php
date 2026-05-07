663162804fc993d533b96613b614871243128246
Nowell VanHoesen <nowell@nvwebdev.com>
2026-05-07 12:18:23 -0400

    added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <link rel="icon" type="image/svg+xml" href="{{ asset('film-slate_3171927.png') }}">
         <title>{{ $page_title }}</title>
         @routes
         @vite(["resources/css/app.css", "resources/js/app.js"])

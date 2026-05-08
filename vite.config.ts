33b1de6246757b54bfd01553db3bda1156695f74
Nowell VanHoesen <nowell@nvwebdev.com>
2026-05-08 08:44:45 -0400

    updated composer packages and added vite plugin vue dev tools to get the crome dev tools vue extension to work properly

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,22 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        vueDevTools(),
+
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+
         vue({
             template: {
                 transformAssetUrls: {
-                    // The Vue plugin will re-write asset URLs, when referenced
-                    // in Single File Components, to point to the Laravel web
-                    // server. Setting this to `null` allows the Laravel plugin
-                    // to instead re-write asset URLs to point to the Vite
-                    // server instead.
                     base: null,
-
-                    // The Vue plugin will parse absolute URLs and treat them
-                    // as absolute paths to files on disk. Setting this to
-                    // `false` will leave absolute URLs un-touched so they can
-                    // reference assets in the public directory as expected.
                     includeAbsolute: false,
                 },
             },
@@ -32,14 +26,8 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            "~fontawesome": path.resolve(
-                __dirname,
-                "node_modules/@fortawesome/fontawesome-free",
-            ),
-            "@": path.resolve(
-                __dirname,
-                "resources/js"
-            ),
+            "~fontawesome": path.resolve(__dirname, "node_modules/@fortawesome/fontawesome-free"),
+            "@": path.resolve(__dirname, "resources/js"),
         },
     },
 });

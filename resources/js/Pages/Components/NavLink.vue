5eb7e107825d3fd207a488370152226aa3c7c2d4
Nowell VanHoesen <nowell@nvwebdev.com>
2026-06-25 14:05:19 -0400

    removed the background color for non-active links which removed the funky border and flattened the non-active links

--- resources/js/Pages/Components/NavLink.vue
+++ resources/js/Pages/Components/NavLink.vue
@@ -16,8 +16,8 @@ defineProps({
 <template>
     <Link
         :class="
-            'text-cold-steel-100 hover:bg-cold-steel-900 focus:bg-cold-steel-900 hover:text-cold-steel-50 block rounded-md px-4 py-3 text-sm font-medium focus:ring focus:inset-ring-blue-300 md:inline-block md:px-3 md:py-2 ' +
-            (active ? 'bg-cold-steel-900' : 'bg-cold-steel-700')
+            'text-cold-steel-100 hover:bg-cold-steel-900 focus:bg-cold-steel-900 hover:text-cold-steel-50 block rounded-md px-4 py-3 text-sm font-medium focus:ring focus:inset-ring-blue-300 md:inline-block md:px-3 md:py-2' +
+            (active ? ' bg-cold-steel-900' : '')
         "
         :aria-current="active ? 'page' : 'false'"
         v-bind="$attrs"

ef07d458be3a6218431593a26152beb87e90111f
Nowell VanHoesen <nowell@nvwebdev.com>
2026-07-08 20:21:09 -0400

    updated the link to hide overflow

--- resources/js/Pages/Components/PosterLink.vue
+++ resources/js/Pages/Components/PosterLink.vue
@@ -4,7 +4,7 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Link v-bind="$attrs" class="group relative mb-auto block max-w-fit rounded-xl">
+    <Link v-bind="$attrs" class="group relative mb-auto block max-w-fit rounded-xl overflow-hidden">
         <slot />
     </Link>
 </template>
